e78f9f50beaf1c98f279366c3fae73f9fa6ea9f4
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-07-08 23:56:47 +0545

    movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,29 +2,13 @@ import React from 'react'
 
 const MovieCard = ({movie : {title,vote_average,poster_path,release_date,original_language},onClick}) => {
   return (
-    <div onClick={onClick} className='movie-card cursor-pointer'>
+    <div onClick={onClick} className='relative movie-card cursor-pointer movie-card group '>
         <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png' } />
-
-
-    <div className='mt-4'>
-        <h3>{title}</h3>
-
-        <div className='content'>
-            <div className='rating'>
-                <img src="star.svg" alt="STAR" />
-
-                <p>{vote_average? vote_average.toFixed(1):"N/A"}</p>
-
-            </div>
-
-            <span>•</span>
-
-            <p className='lang'>{original_language}</p>
-            <span>•</span>
-
-            <p className='year'>{release_date? release_date.split('-')[0]:"N/A"}</p>
+        
+          <div class="absolute inset-0 bg-opacity-40 opacity-0 group-hover:opacity-100 transition duration-300 flex items-center justify-center flex-col space-y-3">
+        <div class="px-4 py-2 bg-white text-black rounded-md font-semibold cursor-pointer">Watch now</div>
+        <div class="px-4 py-2 bg-[#292929b9] text-white rounded-md cursor-pointer">Details</div>
         </div>
-    </div>
 
         </div>
 
