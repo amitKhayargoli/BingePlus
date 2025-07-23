f7852b1a4f3efb81a4e7628abc8521d8a6245bd7
Amit <khayargoliamit99@gmail.com>
2025-07-23 20:35:33 +0545

    formatted the code with prettier

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
+import React from "react";
+import { useNavigate } from "react-router";
 import {
   Home,
   Film,
@@ -8,8 +8,8 @@ import {
   Search,
   User,
   Sun,
-  Moon
-} from 'lucide-react';
+  Moon,
+} from "lucide-react";
 
 import avatar from "/avatar.png";
 
@@ -18,108 +18,110 @@ const Navbar = () => {
 
   return (
     <>
+      <div className="hidden bg-black sticky top-0 z-40 md:flex items-center justify-between px-5 py-3">
+        <h1
+          onClick={() => navigate("/")}
+          class="cursor-pointer text-xl text-gradient"
+        >
+          Binge+
+        </h1>
+        <ul className="hidden text-white md:flex gap-8 ml-20">
+          <li
+            onClick={() => navigate("/movies")}
+            className="cursor-pointer text-sm flex items-center px-3 py-2 rounded-4xl transition-all duration-200 hover:bg-[#0c0c0c]  "
+          >
+            <Film className="w-[18px] mr-1 text-white" />
+            <span className="gray-gradient">Movies</span>
+          </li>
 
-    <div className='hidden bg-black sticky top-0 z-40 md:flex items-center justify-between px-5 py-3'>
-     <h1 onClick={()=>navigate('/')} class="cursor-pointer text-xl text-gradient">
-  Binge+
-</h1>
-<ul className='hidden text-white md:flex gap-8 ml-20'>
-  <li
-    onClick={() => navigate('/movies')}
-    className="cursor-pointer text-sm flex items-center px-3 py-2 rounded-4xl transition-all duration-200 hover:bg-[#0c0c0c]  "
-  >
-    <Film className="w-[18px] mr-1 text-white" />
-    <span className="gray-gradient">Movies</span>
-  </li>
+          <li
+            onClick={() => navigate("/tv")}
+            className="cursor-pointer text-sm flex items-center px-3 py-2 rounded-4xl transition-all duration-200 hover:bg-[#0c0c0c]"
+          >
+            <Tv className="w-[18px] mr-1 text-white" />
+            <span className="gray-gradient">TV Shows</span>
+          </li>
 
-  <li
-    onClick={() => navigate('/tv')}
-    className="cursor-pointer text-sm flex items-center px-3 py-2 rounded-4xl transition-all duration-200 hover:bg-[#0c0c0c]"
-  >
-    <Tv className="w-[18px] mr-1 text-white" />
-    <span className="gray-gradient">TV Shows</span>
-  </li>
+          <li
+            onClick={() => navigate("/watchlist")}
+            className="cursor-pointer text-sm flex items-center px-3 py-2 rounded-4xl transition-all duration-200 hover:bg-[#0c0c0c]"
+          >
+            <Bookmark className="w-[18px] h-[20px] mr-1 text-white" />
+            <span className="gray-gradient">Watchlist</span>
+          </li>
+        </ul>
 
-  <li
-    onClick={() => navigate('/watchlist')}
-    className="cursor-pointer text-sm flex items-center px-3 py-2 rounded-4xl transition-all duration-200 hover:bg-[#0c0c0c]"
-  >
-    <Bookmark className="w-[18px] h-[20px] mr-1 text-white" />
-    <span className="gray-gradient">Watchlist</span>
-  </li>
-</ul>
+        <div className="md:flex items-center gap-5 hidden ">
+          <div className="border-1 rounded-lg flex gap-2 ">
+            <Search
+              onClick={() => {
+                navigate("/search");
+              }}
+              className="w-[22px] h-[22px] text-white cursor-pointer"
+              alt=""
+            />
 
-<div className='md:flex items-center gap-5 hidden '>
-
-<div className='border-1 rounded-lg flex gap-2 '>
-
-  <Search  onClick={()=>{
-    navigate("/search")
-  }} className='w-[22px] h-[22px] text-white cursor-pointer' alt="" />
-
-  {/* <input onClick={()=>{
+            {/* <input onClick={()=>{
     navigate("/search")
   }} type="text" className='outline-none placeholder:text-white text-white cursor-pointer flex-1' placeholder='Start searching' /> */}
-
-
-</div>  
-  <img src={avatar} className='w-[40px]' alt="" />
-</div>
-    </div>
-  
-
+          </div>
+          <img src={avatar} className="w-[40px]" alt="" />
+        </div>
+      </div>
 
       {/* Mobile Header*/}
 
-      <div className='flex justify-between fixed z-50 top-0 left-0 right-0 md:hidden bg-black px-5 py-3'>
-
-            <li className="cursor-pointer text-sm gray-gradient flex items-center"><Bookmark className='w-[18 px] h-[20px] text-white'   alt="" /></li>
-     <h1 onClick={()=>navigate('/')} class="cursor-pointer text-xl text-gradient">
-  Binge+
-</h1>
-            <li className="cursor-pointer text-sm gray-gradient flex items-center"><Sun className='w-[18 px] h-[20px] text-white'   alt="" /></li>
+      <div className="flex justify-between fixed z-50 top-0 left-0 right-0 md:hidden bg-black px-5 py-3">
+        <li className="cursor-pointer text-sm gray-gradient flex items-center">
+          <Bookmark className="w-[18 px] h-[20px] text-white" alt="" />
+        </li>
+        <h1
+          onClick={() => navigate("/")}
+          class="cursor-pointer text-xl text-gradient"
+        >
+          Binge+
+        </h1>
+        <li className="cursor-pointer text-sm gray-gradient flex items-center">
+          <Sun className="w-[18 px] h-[20px] text-white" alt="" />
+        </li>
       </div>
 
       {/* Mobile Bottom Navbar */}
-      <div className='md:hidden fixed bottom-0 left-0 right-0 bg-transparent backdrop-blur-[4px] text-white z-50 shadow-t px-5 py-2 flex justify-between items-center'>
-        
-        
-        <span className='flex flex-col items-center'>
-          <Home onClick={() => navigate('/')} className="w-5 h-5 cursor-pointer " />
-            <h1>Home</h1>
-          </span>
-        <span className='flex flex-col items-center'>
-          <Film onClick={() => navigate('/movies')} className="w-5 h-5 cursor-pointer " />
-            <h1>Movies</h1>
-          </span>
+      <div className="md:hidden fixed bottom-0 left-0 right-0 bg-transparent backdrop-blur-[4px] text-white z-50 shadow-t px-5 py-2 flex justify-between items-center">
+        <span className="flex flex-col items-center">
+          <Home
+            onClick={() => navigate("/")}
+            className="w-5 h-5 cursor-pointer "
+          />
+          <h1>Home</h1>
+        </span>
+        <span className="flex flex-col items-center">
+          <Film
+            onClick={() => navigate("/movies")}
+            className="w-5 h-5 cursor-pointer "
+          />
+          <h1>Movies</h1>
+        </span>
 
-          <span className='flex flex-col items-center'>
+        <span className="flex flex-col items-center">
+          <Tv onClick={() => navigate("/tv")}
+          className="w-5 h-5 cursor-pointer" />
+          <h1>Shows</h1>
+        </span>
 
-        <Tv className="w-5 h-5 cursor-pointer" />
-            <h1>Shows</h1>
+        <span className="flex flex-col items-center">
+          <Search
+            onClick={() => navigate("/search")}
+            className="w-5 h-5 cursor-pointer"
+          />
+          <h1>Search</h1>
+        </span>
 
-          </span>
-
-
-
-
-<span className='flex flex-col items-center'>
-
-        
-        <Search onClick={() => navigate('/search')} className="w-5 h-5 cursor-pointer" />
-               <h1>Search</h1>
-</span>
-
-<span className='flex flex-col items-center'>
-        <User  className="w-5 h-5 cursor-pointer" />
-<h1>User</h1>
-</span>
+        <span className="flex flex-col items-center">
+          <User className="w-5 h-5 cursor-pointer" />
+          <h1>User</h1>
+        </span>
       </div>
-
-
-
-
-      
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,216 +1,222 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { Spinner } from "@material-tailwind/react";
 
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
 import hero from "/thunderbolts.png";
-import { useNavigate } from 'react-router';
-import MovieCard from '../components/MovieCard';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
-import icon from "/icon.png"
-import info from "/Info.png"
-const API_BASE_URL = 'https://api.themoviedb.org/3';
+import { useNavigate } from "react-router";
+import MovieCard from "../components/MovieCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import icon from "/icon.png";
+import info from "/Info.png";
+const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
+    accept: "application/json",
     Authorization: `Bearer ${API_KEY}`,
   },
 };
 
-
 const Home = () => {
-
   const navigate = useNavigate();
   const [isBackdropLoaded, setIsBackdropLoaded] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [topRatedList, setTopRatedList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
- 
   const fetchMovies = async () => {
     setIsLoading(true);
-    setErrorMessage('');
+    setErrorMessage("");
 
     try {
-      const endpoint =`${API_BASE_URL}/trending/movie/day?language=en-US`;
+      const endpoint = `${API_BASE_URL}/trending/movie/day?language=en-US`;
       const endpoint2 = `${API_BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200`;
       const response = await fetch(endpoint, API_OPTIONS);
-      const response2 = await fetch(endpoint2,API_OPTIONS);
+      const response2 = await fetch(endpoint2, API_OPTIONS);
       if (!response.ok) {
-        throw new Error('Failed to fetch movies');
+        throw new Error("Failed to fetch movies");
       }
 
       const data = await response.json();
       const data2 = await response2.json();
-      if (data.Response === 'False') {
+      if (data.Response === "False") {
         setErrorMessage(data.Error);
         setMovieList([]);
         return;
       }
-      if (data.Response2 === 'False') {
+      if (data.Response2 === "False") {
         setErrorMessage(data.Error);
         setTopRatedList([]);
         return;
       }
 
       setMovieList(data.results || []);
-        console.log(movieList);
+      console.log(movieList);
 
       setTopRatedList(data2.results || []);
     } catch (error) {
       console.error(`Error fetching movies:`, error);
-      setErrorMessage('Error fetching movies. Please try again later.');
+      setErrorMessage("Error fetching movies. Please try again later.");
     } finally {
       setIsLoading(false);
     }
   };
 
-useEffect(() => {
+  useEffect(() => {
     fetchMovies();
   }, []);
 
-
-
   return (
     <>
-    <Navbar/>
- <div className="flex flex-col justify-center items-center movie-details text-white bg-[url(/thunderbolts.png)] lg:aspect-auto h-[40vh] bg-cover sm:bg-cover sm:h-[95vh]">
-      {/* <img className='lg:aspect-auto w-auto object-cover ' src={hero} onLoad={() => setIsBackdropLoaded(true)}  alt="" /> */}
+      <Navbar />
+      <div className="flex flex-col justify-center items-center movie-details text-white bg-[url(/thunderbolts.png)] lg:aspect-auto h-[40vh] bg-cover sm:bg-cover sm:h-[95vh]">
+        {/* <img className='lg:aspect-auto w-auto object-cover ' src={hero} onLoad={() => setIsBackdropLoaded(true)}  alt="" /> */}
 
- <div className="p-2 sm:p-5 md:p-10 text-center flex flex-col items-center">
-    <h1 className="text-2xl sm:text-4xl font-bold gray-gradient">Thunderbolts*</h1>
-      <p className='hidden sm:block w-100 md:w-150 text-lg font-extralight'>After finding themselves ensnared in a death trap, seven disillusioned castoffs must embark on a dangerous mission that will force them to confront the darkest corners of their pasts.</p>
-      
-      {/* Buttons */}
-      
-    <div className='flex flex-col sm:flex-row text-sm md:text-md gap-0 md:gap-4 justify-center'>
-        
-      <div onClick={()=>navigate('/watch/986056')} className='mt-2 cursor-pointer flex px-2 py-3 md:p-4 bg-white text-black font-semibold rounded-xl items-center'>
-        <img className='w-[12px] h-[15px] mx-2' src={icon} alt="" />
-        <h1>Watch Now</h1>
+        <div className="p-2 sm:p-5 md:p-10 text-center flex flex-col items-center">
+          <h1 className="text-2xl sm:text-4xl font-bold gray-gradient">
+            Thunderbolts*
+          </h1>
+          <p className="hidden sm:block w-100 md:w-150 text-lg font-extralight">
+            After finding themselves ensnared in a death trap, seven
+            disillusioned castoffs must embark on a dangerous mission that will
+            force them to confront the darkest corners of their pasts.
+          </p>
+
+          {/* Buttons */}
+
+          <div className="flex flex-col sm:flex-row text-sm md:text-md gap-0 md:gap-4 justify-center">
+            <div
+              onClick={() => navigate("/watch/986056")}
+              className="mt-2 cursor-pointer flex px-2 py-3 md:p-4 bg-white text-black font-semibold rounded-xl items-center"
+            >
+              <img className="w-[12px] h-[15px] mx-2" src={icon} alt="" />
+              <h1>Watch Now</h1>
+            </div>
+            <div
+              onClick={() => navigate("/movies/986056")}
+              className="mt-2 cursor-pointer flex px-2 py-3 md:p-4 bg-black text-white font-semibold rounded-xl items-center"
+            >
+              <img className="w-[20px] h-[20px] mx-2" src={info} alt="" />
+              <h1>Details</h1>
+            </div>
+          </div>
+        </div>
       </div>
-      <div onClick={()=>navigate('/movies/986056')}  className='mt-2 cursor-pointer flex px-2 py-3 md:p-4 bg-black text-white font-semibold rounded-xl items-center'>
-        <img className='w-[20px] h-[20px] mx-2' src={info} alt="" />
-        <h1>Details</h1>
-      </div>
-    </div>
-      </div>
 
-    </div>
+      {/* {isBackdropLoaded && ( */}
 
+      <>
+        <section className="all-movies px-5 mb-8 mt-3">
+          <h2 className="md:text-2xl  text-xl text-white font-bold">
+            Trending Now
+          </h2>
 
- {/* {isBackdropLoaded && ( */}
+          {isLoading ? (
+            <Spinner className="h-8 w-8" />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <div>
+              <Swiper
+                modules={[Navigation]}
+                spaceBetween={10}
+                breakpoints={{
+                  320: {
+                    slidesPerView: 2,
+                  },
+                  640: {
+                    slidesPerView: 3,
+                  },
 
-  <>
-       <section className="all-movies px-5 mb-8 mt-3">
-          <h2 className='md:text-2xl  text-xl text-white font-bold'>Trending Now</h2>
+                  768: {
+                    slidesPerView: 3,
+                  },
 
-                {isLoading ? (
-                          <Spinner className="h-8 w-8" />
-                ) : errorMessage ? (
-                    <p className="text-red-500">{errorMessage}</p>
-                ) : (
-                    <div>
-                    <Swiper
-        modules={[Navigation]}
-        spaceBetween={10}
-        breakpoints={{
-            320:{
-                slidesPerView:2,
-            },
-             640: {
-      slidesPerView: 3,
-    },
+                  1024: {
+                    slidesPerView: 4,
+                  },
 
-    768: {
-      slidesPerView: 3,
-    },
-
-    1024: {
-      slidesPerView: 4,
-    },
-
-    1280: {
-      slidesPerView: 5,
-    },
-        }}
-        navigation
-        loop={movieList.length > 5} 
-        className="mySwiper"
-        >
-        {movieList.map((movie) => (
-            <SwiperSlide key={movie.id}>
-            <MovieCard
-                movie={movie}
-                onClick={() => navigate(`/movies/${movie.id}`)}
-            />
-            </SwiperSlide>
-        ))}
-        </Swiper>
+                  1280: {
+                    slidesPerView: 5,
+                  },
+                }}
+                navigation
+                loop={movieList.length > 5}
+                className="mySwiper"
+              >
+                {movieList.map((movie) => (
+                  <SwiperSlide key={movie.id}>
+                    <MovieCard
+                      movie={movie}
+                      onClick={() => navigate(`/movies/${movie.id}`)}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
             </div>
           )}
         </section>
- 
-       <section className="all-movies px-5">
-          <h2 className='md:text-2xl text-xl text-white font-bold'>Top Rated</h2>
 
-                {isLoading ? (
-      <Spinner className="h-8 w-8" />
+        <section className="all-movies px-5">
+          <h2 className="md:text-2xl text-xl text-white font-bold">
+            Top Rated
+          </h2>
 
-                ) : errorMessage ? (
-                    <p className="text-red-500">{errorMessage}</p>
-                ) : (
-                    <div>
-                    <Swiper
-        modules={[Navigation]}
-        spaceBetween={10}
-       breakpoints={{
-            320:{
-                slidesPerView:2,
-            },
-             640: {
-      slidesPerView: 3,
-    },
+          {isLoading ? (
+            <Spinner className="h-8 w-8" />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <div>
+              <Swiper
+                modules={[Navigation]}
+                spaceBetween={10}
+                breakpoints={{
+                  320: {
+                    slidesPerView: 2,
+                  },
+                  640: {
+                    slidesPerView: 3,
+                  },
 
-    768: {
-      slidesPerView: 3,
-    },
+                  768: {
+                    slidesPerView: 3,
+                  },
 
-    1024: {
-      slidesPerView: 4,
-    },
+                  1024: {
+                    slidesPerView: 4,
+                  },
 
-    1280: {
-      slidesPerView: 5,
-    },
-        }}
-        navigation
-        loop={topRatedList.length > 5} 
-        className="mySwiper"
-        >
-        {topRatedList.map((movie) => (
-            <SwiperSlide key={movie.id}>
-            <MovieCard
-                movie={movie}
-                onClick={() => navigate(`/movies/${movie.id}`)}
-            />
-            </SwiperSlide>
-        ))}
-        </Swiper>
+                  1280: {
+                    slidesPerView: 5,
+                  },
+                }}
+                navigation
+                loop={topRatedList.length > 5}
+                className="mySwiper"
+              >
+                {topRatedList.map((movie) => (
+                  <SwiperSlide key={movie.id}>
+                    <MovieCard
+                      movie={movie}
+                      onClick={() => navigate(`/movies/${movie.id}`)}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
             </div>
           )}
         </section>
-        </>
- {/* )}; */}
-
+      </>
+      {/* )}; */}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
