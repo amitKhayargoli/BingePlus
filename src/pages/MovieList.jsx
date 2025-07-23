5ce5aee3f3f7dcf1b91b5eb562ce3a1b212ebdd4
Amit <khayargoliamit99@gmail.com>
2025-07-23 20:36:26 +0545

    minor changes to the tv details

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from 'react';
-import { useDebounce } from 'react-use';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useDebounce } from "react-use";
+import { useNavigate } from "react-router-dom";
 
-import MovieCard from '../components/MovieCard';
+import MovieCard from "../components/MovieCard";
 import { Spinner } from "@material-tailwind/react";
-import '../index.css'; // optional for styles
-import Navbar from '../components/Navbar';
+import "../index.css"; // optional for styles
+import Navbar from "../components/Navbar";
 
-const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
+    accept: "application/json",
     Authorization: `Bearer ${API_KEY}`,
   },
 };
@@ -21,17 +21,17 @@ const API_OPTIONS = {
 const MovieList = () => {
   const navigate = useNavigate();
 
-  const [searchTerm, setSearchTerm] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
 
   useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
-  const fetchMovies = async (query = '') => {
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
-    setErrorMessage('');
+    setErrorMessage("");
 
     try {
       const endpoint = query
@@ -41,12 +41,12 @@ const MovieList = () => {
       const response = await fetch(endpoint, API_OPTIONS);
 
       if (!response.ok) {
-        throw new Error('Failed to fetch movies');
+        throw new Error("Failed to fetch movies");
       }
 
       const data = await response.json();
 
-      if (data.Response === 'False') {
+      if (data.Response === "False") {
         setErrorMessage(data.Error);
         setMovieList([]);
         return;
@@ -55,7 +55,7 @@ const MovieList = () => {
       setMovieList(data.results || []);
     } catch (error) {
       console.error(`Error fetching movies:`, error);
-      setErrorMessage('Error fetching movies. Please try again later.');
+      setErrorMessage("Error fetching movies. Please try again later.");
     } finally {
       setIsLoading(false);
     }
@@ -67,37 +67,33 @@ const MovieList = () => {
 
   return (
     <>
-    <Navbar/>
-    <main className="pt-10 md:pt-0">
-      <div className="wrapper">
-        <header className="flex items-center">
+      <Navbar />
+      <main className="pt-10 md:pt-0">
+        <div className="wrapper">
+          <header className="flex items-center"></header>
 
+          <section className="all-movies px-10 md:px-20 text-white">
+            <h1 className="text-4xl font-bold">Movies</h1>
 
-        </header>
-
-        <section className="all-movies px-10 md:px-20 text-white">
-          <h1 className='text-4xl font-bold'>Movies</h1>
-
-          {isLoading ? (
-       <Spinner className="h-8 w-8 fixed top-1/2 left-1/2" />
-
-          ) : errorMessage ? (
-            <p className="text-red-500">{errorMessage}</p>
-          ) : (
-            <ul>
-              {movieList.map((movie) => (
-                <MovieCard
-                key={movie.id}
-                movie={movie}
-                onClick={() => navigate(`/movies/${movie.id}`)}
-                />
-              ))}
-            </ul>
-          )}
-        </section>
-      </div>
-    </main>
-          </>
+            {isLoading ? (
+              <Spinner className="h-8 w-8 fixed top-1/2 left-1/2" />
+            ) : errorMessage ? (
+              <p className="text-red-500">{errorMessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <MovieCard
+                    key={movie.id}
+                    movie={movie}
+                    onClick={() => navigate(`/movies/${movie.id}`)}
+                  />
+                ))}
+              </ul>
+            )}
+          </section>
+        </div>
+      </main>
+    </>
   );
 };
 
