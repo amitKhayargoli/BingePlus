608da7314e4a85070482d3ec314d771b86fc6de6
Amit <khayargoliamit99@gmail.com>
2025-07-25 02:36:48 +0545

    Created a simple CardSlider Component for the homepage

---
+++ src/components/CardSlider.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import MovieCard from "./MovieCard";
+import { Spinner } from "@material-tailwind/react";
+import { useNavigate } from 'react-router';
+
+const CardSlider = ({title, movieList, tvList, isLoading, errorMessage}) => {
+
+    const navigate = useNavigate();
+  return (
+    <section className="all-movies px-5">
+    <h2 className="md:text-2xl text-xl text-white font-bold">
+      {title}
+    </h2>
+
+    {isLoading ? (
+      <Spinner className="h-8 w-8" />
+    ) : errorMessage ? (
+      <p className="text-red-500">{errorMessage}</p>
+    ) : (
+      <div>
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={10}
+          breakpoints={{
+            320: {
+              slidesPerView: 2,
+            },
+            640: {
+              slidesPerView: 3,
+            },
+            768: {
+              slidesPerView: 3,
+            },
+            1024: {
+              slidesPerView: 4,
+            },
+            1280: {
+              slidesPerView: 5,
+            },
+          }}
+          navigation
+          loop={movieList && movieList.length > 5 || tvList && tvList.length > 5}
+          className="mySwiper"
+        >
+
+        {movieList && 
+          (movieList.map((media) => (
+            <SwiperSlide key={media.id}>
+              <MovieCard
+                movie={media}
+                onClick={() => navigate(`/movies/${media.id}`)}
+              />
+            </SwiperSlide>
+          )))}
+
+          {tvList && (
+            tvList.map((media) => (
+              <SwiperSlide key={media.id}>
+                <MovieCard
+                  movie={media}
+                  onClick={() => navigate(`/tv/${media.id}`)}
+                />
+              </SwiperSlide>
+            ))
+          )}
+
+        </Swiper>
+      </div>
+    )}
+  </section>
+  )
+}
+
+export default CardSlider
