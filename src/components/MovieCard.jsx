7217ab2a84037148a2b82481e6e2c3be4a925634
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-08-16 00:08:47 +0545

    movie card blur

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,45 +1,24 @@
-import React, { useState } from "react";
-import { Blurhash } from "react-blurhash";
+import React from "react";
+import BlurImage from "./BlurImage";
 
 const MovieCard = ({
-  movie: { title, vote_average, poster_path, release_date, original_language, blur_hash },
+  movie: { title, vote_average, poster_path, release_date, original_language },
   onClick,
 }) => {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
-  const handleImageLoad = () => {
-    setImageLoaded(true);
-  };
+  const imageSrc = poster_path
+    ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+    : "/no-movie.png";
 
   return (
     <div
       onClick={onClick}
-      className="relative movie-card cursor-pointer movie-card group "
+      className="relative movie-card cursor-pointer movie-card group overflow-hidden rounded-lg"
     >
-      {!imageLoaded && blur_hash && (
-        <Blurhash
-          hash={blur_hash}
-          width="100%"
-          height="100%"
-          resolutionX={32}
-          resolutionY={32}
-          punch={1}
-          className="absolute inset-0"
-        />
-      )}
-      <img
-        loading="lazy"
-        src={
-          poster_path
-            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-            : "/no-movie.png"
-        }
-        onLoad={handleImageLoad}
-        className={`w-full h-full object-cover ${!imageLoaded && blur_hash ? "opacity-0" : "opacity-100"}`}
-        style={{ transition: "opacity 0.3s ease-in-out" }}
-      />
+      <div className="w-full aspect-[2/3] overflow-hidden">
+        <BlurImage src={imageSrc} alt={title} />
+      </div>
 
-      <div className="absolute inset-0 bg-opacity-40 opacity-0 group-hover:opacity-100 transition duration-300 flex items-center justify-center flex-col space-y-3">
+      <div className="absolute inset-0 bg-black bg-opacity-0 opacity-0 group-hover:bg-opacity-40 group-hover:opacity-100 transition duration-300 flex items-center justify-center flex-col space-y-3">
         <div className="px-4 py-2 bg-[var(--movie-cardbtn-primary)] text-black rounded-md font-semibold cursor-pointer">
           Watch now
         </div>
