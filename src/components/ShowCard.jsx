5623318f4d6b9d701a28009f127de99741ac3fb5
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-08-16 00:08:56 +0545

    same for show card

--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import BlurImage from "./BlurImage";
 
-const ShowCard = ({ show: { poster_path }, onClick }) => {
+const ShowCard = ({ show: { poster_path, name }, onClick }) => {
+  const imageSrc = poster_path
+    ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+    : "/no-movie.png";
+
   return (
     <div
       onClick={onClick}
-      className="relative movie-card cursor-pointer movie-card group "
+      className="relative movie-card cursor-pointer movie-card group"
     >
-      <img
-        loading="lazy"
-        src={
-          poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : ""
-        }
-      />
+      <div className="w-full aspect-[2/3]">
+        <BlurImage src={imageSrc} alt={name} />
+      </div>
 
       <div class="absolute inset-0 bg-opacity-40 opacity-0 group-hover:opacity-100 transition duration-300 flex items-center justify-center flex-col space-y-3">
         <div class="px-4 py-2 bg-white text-black rounded-md font-semibold cursor-pointer">
